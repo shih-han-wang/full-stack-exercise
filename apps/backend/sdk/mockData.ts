bca7078088d51ea080067a81b3bf03c955cd8808
edxv <ed@incred.co.uk>
2022-09-13 13:41:26 +0100

    update mock data

--- apps/backend/sdk/mockData.ts
+++ apps/backend/sdk/mockData.ts
@@ -9,21 +9,21 @@ export const mockQuestions: Array<
     answer_type: Questions_Answer_Type_Enum.Text,
     input_label: 'What nut is used to make marzipan?',
     input_options: ['Almond', 'Cashew', 'Hazelnut', 'Pecan'],
-    difficulty: 0.5
+    difficulty: 0.3
   },
   {
     id: 'question2',
     answer_value: 'Canberra',
     answer_type: Questions_Answer_Type_Enum.Text,
     input_label: 'What is the capital of Australia?',
-    difficulty: 0.5
+    difficulty: 0.3
   },
   {
     id: 'question3',
     answer_value: '42',
     answer_type: Questions_Answer_Type_Enum.Number,
     input_label: 'What is 6 * 7?',
-    difficulty: 0.5
+    difficulty: 0.3
   },
   {
     id: 'question4',
@@ -31,28 +31,28 @@ export const mockQuestions: Array<
     answer_type: Questions_Answer_Type_Enum.Text,
     input_label: 'Which cheese comes from the Netherlands?',
     input_options: ['Cheddar', 'Emmental', 'Gouda', 'Mozzarella'],
-    difficulty: 0.5
+    difficulty: 0.3
   },
   {
     id: 'question5',
     answer_value: '125',
     answer_type: Questions_Answer_Type_Enum.Number,
     input_label: 'What is 500 / 4?',
-    difficulty: 0.5
+    difficulty: 0.3
   },
   {
     id: 'question6',
     answer_value: 'Oxygen',
     answer_type: Questions_Answer_Type_Enum.Text,
     input_label: "What element does 'O' represent on the periodic table?",
-    difficulty: 0.5
+    difficulty: 1
   },
   {
     id: 'question7',
     answer_value: '50',
     answer_type: Questions_Answer_Type_Enum.Number,
     input_label: 'In meters, how long is an Olympic swimming pool?',
-    difficulty: 0.5
+    difficulty: 1
   },
   {
     id: 'question8',
@@ -60,7 +60,7 @@ export const mockQuestions: Array<
     answer_type: Questions_Answer_Type_Enum.Text,
     input_label:
       'Which English town has football teams called United and Wednesday?',
-    difficulty: 0.5
+    difficulty: 1
   },
   {
     id: 'question9',
@@ -68,13 +68,13 @@ export const mockQuestions: Array<
     answer_type: Questions_Answer_Type_Enum.Text,
     input_label: 'What is the highest mountain in the world?',
     input_options: ['Everest', 'K2', 'Mount Fuji', 'Mount Kilimanjaro'],
-    difficulty: 0.5
+    difficulty: 1
   },
   {
     id: 'question10',
     answer_value: 'Madrid',
     answer_type: Questions_Answer_Type_Enum.Text,
     input_label: 'What is the capital of Spain?',
-    difficulty: 0.5
+    difficulty: 1
   }
 ]
